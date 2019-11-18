b91cfad20a1dbcef6cd0db31a813051810c0a5ad
divbeech <dyadav1006@gmail.com>
2019-11-17 20:20:44 -0500

    Done.

--- src/ExampleComponent.js
+++ src/ExampleComponent.js
@@ -8,8 +8,11 @@ class ExampleComponent extends Component {
         <img src="https://s3.amazonaws.com/ironboard-learn/sunglasses.gif" alt="whoa"/>
         <p>Whoa!</p>
       </div>
+      
     );
   }
 }
+
+
 
 export default ExampleComponent;
